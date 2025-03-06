d1f1dcbba25a0249a9aefa4738dac12de7dec055
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:22:03 -0300

    Add files via upload

---
+++ Exercicio1071/Program.cs
@@ -0,0 +1,27 @@
+﻿int x;
+int y;
+int i;
+int temp;
+int soma = 0;
+
+Console.WriteLine("Escreva seu numero:");
+x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Escreva seu numero:");
+y = int.Parse(Console.ReadLine());
+
+if (x > y)
+{
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+for (i = x + 1; i < y; i++)
+{
+    if (i % 2 != 0);
+    {
+        soma += i;
+    }
+    Console.WriteLine(soma);
+}
