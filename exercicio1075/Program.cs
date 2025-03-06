2d6c688ffb7dcc003f6d9f04d7dffc0c7771005a
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:24:59 -0300

    Add files via upload

---
+++ exercicio1075/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+int i;
+Console.WriteLine("escreva um valor: ");
+n =int.Parse(Console.ReadLine());
+
+for (i = 1; i <= 10000; i++)
+{
+    if (i % n == 2)
+    {
+        Console.WriteLine(i);
+    }
+}
