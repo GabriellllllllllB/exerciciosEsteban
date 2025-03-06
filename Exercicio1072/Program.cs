59678640cfc5ffbf0e008802bd8fb0aad27c8986
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:22:32 -0300

    Add files via upload

---
+++ Exercicio1072/Program.cs
@@ -0,0 +1,28 @@
+﻿int x;
+int i;
+int n;
+int dentro = 0;
+int fora = 0;
+
+Console.WriteLine("Escreva quantos valores serão lidos: ");
+n = int.Parse(Console.ReadLine());
+
+
+for (i = 0; i < n; i++)
+        {
+    Console.WriteLine("Escreva um valor: ");
+    x = int.Parse(Console.ReadLine());
+
+if (x >= 10 && x <= 20) 
+            {
+dentro++;
+            }
+else
+            {
+fora++;
+            }
+        }
+Console.WriteLine($"{dentro} in");
+Console.WriteLine($"{fora} out");
+
+
