eb12baf2a4effb1edfc4560efb61240108008df5
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:31:11 -0300

    Add files via upload

---
+++ exercicio1156/Program.cs
@@ -0,0 +1,14 @@
+﻿double s = 0;
+int i;
+double n;
+double d;
+
+for (i = 1; i <= 20; i++)
+{
+    n = 2 * i - 1;
+    d = Math.Pow(2, i - 1);
+
+    s += n / d;
+}
+
+Console.WriteLine($"{s:F2}");
---
+++ exercicio1157/Program.cs
@@ -0,0 +1,19 @@
+﻿int n;
+int i;
+int div = 0;
+
+Console.WriteLine("escreva seu numero: ");
+n = int.Parse(Console.ReadLine());
+
+for  (i = 1; i < n; i++)
+{
+    div = n % i;
+    if (div == 0)
+    {
+        Console.WriteLine(i);
+
+    }
+   
+}
+Console.WriteLine(n);
+
