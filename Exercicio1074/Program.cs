9cccc107757333308018b2d7a06e9696b358e21d
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:24:12 -0300

    Add files via upload

---
+++ Exercicio1074/Program.cs
@@ -0,0 +1,28 @@
+﻿int x;
+int i;
+int n;
+Console.WriteLine("Escreva quantos valores serão lidos");
+n = int.Parse(Console.ReadLine());
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("Escreva um valor: ");
+    x = int.Parse(Console.ReadLine());
+
+    if ((x % 2 == 0) && (x > 0))
+    {
+        Console.WriteLine("par e positivo");
+    }
+    else if ((x % 2 == 0) && (x < 0))
+    {
+        Console.WriteLine("par e negativo");
+    }
+    else if ((x % 2 != 0) && (x > 0))
+    {
+        Console.WriteLine("impar e positivo");
+    }
+    else if ((x % 2 != 0) && (x < 0))
+    {
+        Console.WriteLine("impar e negativo");
+    }
+}
