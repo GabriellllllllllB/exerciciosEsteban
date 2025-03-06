73979e421d0d744dab7abf2e7b664ca26b9a774f
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-06 16:25:45 -0300

    Add files via upload

---
+++ Exercicio1079/Program.cs
@@ -0,0 +1,22 @@
+﻿double x1;
+double x2;
+double x3;
+int i;
+int n;
+double m;
+Console.WriteLine("Escreva quantas notas serão lidos");
+n = int.Parse(Console.ReadLine());
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("Escreva a nota: ");
+    x1 = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+    Console.WriteLine("Escreva a nota: ");
+    x2 = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+    Console.WriteLine("Escreva a nota: ");
+    x3 = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+    m = (x1 * 2 + x2 * 3 + x3 * 5) / (10);
+
+    Console.WriteLine("A média é: " + m);
+}
---
+++ exercicio1080/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int maior = 0;
+int posicao = 0;
+int n;
+int i;
+
+Console.WriteLine("Escreva seus 100 numeros");
+for (i = 1; i <= 100; i++)
+{
+    n = int.Parse(Console.ReadLine());
+
+    if (n > maior)
+{
+     maior = n;
+     posicao = i; 
+    }
+}
+        Console.WriteLine(maior);
+        Console.WriteLine(posicao);
+   
